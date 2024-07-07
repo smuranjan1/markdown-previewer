5a3875915ab202ad881cf35736fcea651595be6b
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-07 12:39:37 -0500

    Passes all testcases.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const startText = `
+# h1 Heading
+## h2 Subheading
+[Google](https://www.google.com)\n
+\`Hello World!\`
+- 1
+- 2
+- 3
+> Block Quotes!
+\`\`\`
+ console.log("Hello World!");
+\`\`\`
+![Image](https://mdg.imgix.net/assets/images/tux.png?auto=format&fit=clip&q=40&w=100)
 
-export default App;
+**bold**
+`;
+
+export function App() {
+    const [input, setInput] = useState(startText);
+
+  return (
+    <>
+        <h1 id="title">Markdown Previewer</h1>
+        <span id="textarea">
+        <textarea id="editor" type="text" rows="40" cols="50" value={input} onChange={(e) => setInput(e.target.value)} />
+        </span>
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked.parse(input)}}></div>
+    </>
+  );
+} 
